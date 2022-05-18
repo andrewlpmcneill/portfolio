e10dd18ef2f816545d9b1bdb69a5be2dd9cb9180
Andrew McNeill <andrew@andrewmc.ca>
2022-05-18 20:14:59 -0300

    adds component content to Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
 
 export default function Nav() {
   return (
-    <div>Nav</div>
+    <nav>
+      <a className='nav-title-link' href='/'>
+        <h1 className='nav-title'>ANDREWM</h1><h1 className='nav-title-coloured'>C</h1>
+      </a>
+      <div className='nav-links'>
+        <a className='nav-link nav-link-about' href='/about'>
+          <h2 className='nav-link'>About</h2>
+        </a>
+        <a className='nav-link nav-link-projects' href='/projects'>
+          <h2 className='nav-link'>Projects</h2>
+        </a>
+        <a className='nav-link nav-link-contact' href='/contact'>
+          <h2 className='nav-link'>Contact</h2>
+        </a>
+        <a className='nav-link-icon nav-link-icon-gh' href='https://github.com/andrewlpmcneill'>
+          <AiFillGithub />
+        </a>
+        <a className='nav-link-icon nav-link-icon-li' href='https://www.linkedin.com/in/andrewlpmcneill/'>
+          <AiFillLinkedin />
+        </a>
+      </div>
+    </nav>
   )
 }
