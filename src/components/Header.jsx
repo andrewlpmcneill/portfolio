b2283baec53e1c195844b3286cc8fc25f54f94f6
Andrew McNeill <andrew@andrewmc.ca>
2022-05-18 20:14:45 -0300

    adds component content to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <>
+      <h1 className='header-title'>
+        A developer who cares about detail.
+      </h1>
+      <div className='header-links'>
+        <a className='header-link header-link-reach' href='/contact'>
+          <h2 className='header-link-title'>Reach Out →</h2>
+        </a>
+        <a className='header-link header-link-view' href='/projects'>
+          <h2 className='header-link-title'>View Projects ↓</h2>
+        </a>
+      </div>
+    </>
   )
 }
