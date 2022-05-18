335872dd8268b6f9677c67d2d6985ee4090ec3e2
Andrew McNeill <andrew@andrewmc.ca>
2022-05-18 14:59:27 -0300

    creates Layout wrapper component

---
+++ src/components/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from './Nav';
+import Header from './Header';
+import Footer from './Footer';
+
+export default function Layout() {
+  return (
+    <>
+      <Nav />
+      <Header />
+      <Footer />
+    </>
+  )
+}
