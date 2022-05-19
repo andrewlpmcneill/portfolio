ac9e3958f1edf09fedc49fb46d62003363ae9fde
Andrew McNeill <andrew@andrewmc.ca>
2022-05-19 11:39:46 -0300

    adds content to Nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,28 +2,43 @@ import React from 'react';
 import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
 
 export default function Nav() {
+  
   return (
+
     <nav>
-      <a className='nav-title-link' href='/'>
-        <h1 className='nav-title'>ANDREWM</h1><h1 className='nav-title-coloured'>C</h1>
+      <a className='title-link' href='/'>
+        <h1 className='title'>
+          ANDREWM
+        </h1>
+        <h1 className='title-coloured'>
+          C
+        </h1>
       </a>
-      <div className='nav-links'>
-        <a className='nav-link nav-link-about' href='/about'>
-          <h2 className='nav-link'>About</h2>
+      <div className='links'>
+        <a className='link link-about' href='/about'>
+          <h2 className='link'>
+            About
+          </h2>
         </a>
-        <a className='nav-link nav-link-projects' href='/projects'>
-          <h2 className='nav-link'>Projects</h2>
+        <a className='link link-projects' href='/projects'>
+          <h2 className='link'>
+            Projects
+          </h2>
         </a>
-        <a className='nav-link nav-link-contact' href='/contact'>
-          <h2 className='nav-link'>Contact</h2>
+        <a className='link link-contact' href='/contact'>
+          <h2 className='link'>
+            Contact
+          </h2>
         </a>
-        <a className='nav-link-icon nav-link-icon-gh' href='https://github.com/andrewlpmcneill'>
+        <a className='link-icon link-icon-gh' href='https://github.com/andrewlpmcneill'>
           <AiFillGithub />
         </a>
-        <a className='nav-link-icon nav-link-icon-li' href='https://www.linkedin.com/in/andrewlpmcneill/'>
+        <a className='link-icon link-icon-li' href='https://www.linkedin.com/in/andrewlpmcneill/'>
           <AiFillLinkedin />
         </a>
       </div>
     </nav>
+  
   )
+
 }
