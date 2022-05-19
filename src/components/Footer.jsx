34afeccba5f93723985108748a8f99a9dc54d37c
Andrew McNeill <andrew@andrewmc.ca>
2022-05-19 11:38:47 -0300

    adds content to Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,17 +3,27 @@ import { RiGatsbyFill } from 'react-icons/ri';
 import { AiFillGithub } from 'react-icons/ai';
 
 export default function Footer() {
+
   return (
+
     <footer>
-      <h1 className='footer-title'>Made by Andrew McNeill</h1>
-      <div className='footer-links'>
-        <a className='footer-link footer-link-gatsby' href='https://www.gatsbyjs.com/'>
-          <h1 className='footer-link-title'>Gatsby <RiGatsbyFill /></h1>
+      <h1 className='title'>
+        Made by Andrew McNeill
+      </h1>
+      <div className='links'>
+        <a className='link link-gatsby' href='https://www.gatsbyjs.com/'>
+          <h1 className='link-title'>
+            Gatsby <RiGatsbyFill />
+          </h1>
         </a>
-        <a className='footer-link footer-link-github' href='https://github.com/andrewlpmcneill'>
-          <h1 className='footer-link-title'>Github <AiFillGithub /></h1>
+        <a className='link link-github' href='https://github.com/andrewlpmcneill'>
+          <h1 className='link-title'>
+            GitHub <AiFillGithub />
+          </h1>
         </a>
       </div>
     </footer>
+  
   )
+  
 }
