bd09a91bbac9b90f9c82e694fd668493639f9528
Andrew McNeill <andrew@andrewmc.ca>
2022-05-19 23:57:15 -0300

    adds content to 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from 'react';
+import Nav from '../components/Nav';
+import Footer from '../components/Footer';
 
 export default function Error() {
+
   return (
-    <div>Error Page</div>
+
+    <>
+      <Nav />
+      <header>
+        <h1 className='title'>
+          Oops! Looks like <br /><span className='title-coloured'>you&apos;ve taken a wrong turn.</span>
+        </h1>
+        <div className='links'>
+          <a className='link link-home' href='/'>
+            <h2 className='link-title'>
+              Home ←
+            </h2>
+          </a>
+        </div>
+    </header>
+      <Footer />
+    </>
+  
   )
+
 }
