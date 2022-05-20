f524ac80e3f724bb0c63a0d5af2b1b2789c52594
Andrew McNeill <andrew@andrewmc.ca>
2022-05-19 23:58:33 -0300

    removes header component, replaces with main

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,15 +7,13 @@ export default function Projects() {
   return (
 
     <Layout>
-      <header>
+      <main>
         <h1 className='header-heading'>
           Projects
         </h1>
         <h3 className='header-subheading'>
           View them all <a className='header-subheading-link' href='https://github.com/andrewlpmcneill?tab=repositories'>on GitHub</a>
         </h3>
-      </header>
-      <main>
         <ProjectsList />
       </main>
     </Layout>
