ee1a3c1afadb6421ebe6dd9315097c6e958b5182
Andrew McNeill <andrew@andrewmc.ca>
2022-05-19 23:55:51 -0300

    creates navbar Dropdown component

---
+++ src/components/Nav.Dropdown.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Nav.Dropdown.css'
+
+export default function Dropdown(props) {
+
+  const {
+    open
+  } = props;
+
+  return (
+
+    <section
+      className="p-menu1"
+    >
+      <nav
+        className="navigation"
+        role="navigation"
+      >
+        <nav
+          className={open ? "menu1 open" : "menu1"}
+        >
+          <a
+            className="dropdown-link dropdown-link1"
+            href="/about"
+          >
+            <p className='dropdown-link-text dropdown-link-text1'>About</p>
+          </a>
+          <a
+            className="dropdown-link dropdown-link2"
+            href="/projects"
+          >
+            <p className='dropdown-link-text dropdown-link-text2'>Projects</p>
+          </a>
+          <a
+            className="dropdown-link dropdown-link3"
+            href="/contact"
+          >
+            <p className='dropdown-link-text dropdown-link-text2'>Contact</p>
+          </a>
+        </nav>
+      </nav>
+    </section>
+  
+  )
+
+}
