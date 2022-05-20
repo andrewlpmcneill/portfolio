21fc3ecaf4de4f2c40ec30b5afa1f2622149d85f
Andrew McNeill <andrew@andrewmc.ca>
2022-05-19 23:56:34 -0300

    adds imports, additional content to Nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,43 +1,62 @@
 import React from 'react';
-import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
+import { useState } from 'react';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+import Dropdown from './Nav.Dropdown';
+import Hamburger from './Nav.hamburger';
+import { Header, Navbar, Div, HamburgerWrapper, TitleLink, Link, LinkIcon, H1, H2, NavbarWrapper } from './Nav.styles';
 
 export default function Nav() {
+
+  const [open, setOpen] = useState(false);
   
   return (
 
-    <nav>
-      <a className='title-link' href='/'>
-        <h1 className='title'>
-          ANDREWM
-        </h1>
-        <h1 className='title-coloured'>
-          C
-        </h1>
-      </a>
-      <div className='links'>
-        <a className='link link-about' href='/about'>
-          <h2 className='link'>
-            About
-          </h2>
-        </a>
-        <a className='link link-projects' href='/projects'>
-          <h2 className='link'>
-            Projects
-          </h2>
-        </a>
-        <a className='link link-contact' href='/contact'>
-          <h2 className='link'>
-            Contact
-          </h2>
-        </a>
-        <a className='link-icon link-icon-gh' href='https://github.com/andrewlpmcneill'>
-          <AiFillGithub />
-        </a>
-        <a className='link-icon link-icon-li' href='https://www.linkedin.com/in/andrewlpmcneill/'>
-          <AiFillLinkedin />
-        </a>
-      </div>
-    </nav>
+    <Header>
+      <NavbarWrapper>
+        <Navbar>
+          <TitleLink href='/'>
+            <H1>
+              ANDREW
+            </H1>
+            <H1 clr={true}>
+              MC
+            </H1>
+          </TitleLink>
+          <Div>
+            <Link href='/about'>
+              <H2 className='link'>
+                About
+              </H2>
+            </Link>
+            <Link href='/projects'>
+              <H2 className='link'>
+                Projects
+              </H2>
+            </Link>
+            <Link href='/contact'>
+              <H2 className='link'>
+                Contact
+              </H2>
+            </Link>
+            <LinkIcon href='https://github.com/andrewlpmcneill'>
+              <AiFillGithub />
+            </LinkIcon>
+            <LinkIcon href='https://www.linkedin.com/in/andrewlpmcneill/'>
+              <AiFillLinkedin />
+            </LinkIcon>
+            <HamburgerWrapper>
+              <Hamburger
+                open={open}
+                setOpen={setOpen}
+              />
+            </HamburgerWrapper>
+          </Div>
+        </Navbar>
+      </NavbarWrapper>
+      <Dropdown
+        open={open}
+      />
+    </Header>
   
   )
 
