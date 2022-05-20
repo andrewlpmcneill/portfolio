a5bf52e31c5af3ae6496c6a71bf0a80915f19a33
Andrew McNeill <andrew@andrewmc.ca>
2022-05-19 23:58:13 -0300

    adds tag to home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,37 +10,39 @@ export default function Home() {
   return (
   
     <Layout>
-      <aside className='technologies'>
-        <h3 className='technologies-react'>
-          <RiReactjsFill /> React
-        </h3>
-        <h3 className='technologies-node'>
-          <DiNodejs />
-        </h3>
-        <h3 className='technologies-express'>
-          Express
-        </h3>
-        <h3 className='technologies-rails'>
-          <SiRubyonrails />
-        </h3>
-        <h3 className='technologies-postgres'>
-          PostgreSQL
-        </h3>
-        <h3 className='technologies-mongo'>
-          MongoDB
-        </h3>
-      </aside>
-      <header className='introduction'>
-        <h1 className='introduction-heading'>
-          Based in Halifax, Nova Scotia
-        </h1>
-        <p className='introduction-paragraph'>
-          I spend my free time cycling, sailing, and having fun with microcontrollers.
-        </p>
-      </header>
-      <section className='skills'>
-        <Skills />
-      </section>
+      <main>
+        <aside className='technologies'>
+          <h3 className='technologies-react'>
+            <RiReactjsFill /> React
+          </h3>
+          <h3 className='technologies-node'>
+            <DiNodejs />
+          </h3>
+          <h3 className='technologies-express'>
+            Express
+          </h3>
+          <h3 className='technologies-rails'>
+            <SiRubyonrails />
+          </h3>
+          <h3 className='technologies-postgres'>
+            PostgreSQL
+          </h3>
+          <h3 className='technologies-mongo'>
+            MongoDB
+          </h3>
+        </aside>
+        <header className='introduction'>
+          <h1 className='introduction-heading'>
+            Based in Halifax, Nova Scotia
+          </h1>
+          <p className='introduction-paragraph'>
+            I spend my free time cycling, sailing, and having fun with microcontrollers.
+          </p>
+        </header>
+        <section className='skills'>
+          <Skills />
+        </section>
+      </main>
     </Layout>
   
   )
