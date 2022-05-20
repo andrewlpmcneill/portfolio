a27b7fe9cd8cc4dd81ce969edb226bc33fa22144
Andrew McNeill <andrew@andrewmc.ca>
2022-05-19 23:57:53 -0300

    adds content to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Layout from '../components/Layout';
 
 export default function Contact() {
+
   return (
-    <div>Contact</div>
+
+    <Layout>
+      <main>
+        <h1 className='heading'>
+          Contact
+        </h1>
+        <p className='paragraph'>
+          <a className='paragraph-link' href='andrew@andrewmc.ca'>andrew@andrewmc.ca</a>
+        </p>
+        <p className='paragraph'>
+          <a className='paragraph-link' href='https://github.com/andrewlpmcneill'>GitHub</a>
+        </p>
+        <p className='paragraph'>
+          <a className='paragraph-link' href='https://www.linkedin.com/in/andrewlpmcneill/'>LinkedIn</a>
+        </p>
+      </main>
+    </Layout>
+
   )
+
 }
