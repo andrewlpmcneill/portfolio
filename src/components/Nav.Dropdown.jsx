92b03d8e776f3819a6d0f121833575bd4d9e0859
Andrew McNeill <andrew@andrewmc.ca>
2022-05-22 20:40:20 -0300

    adds page prop and conditionally styles headings

--- src/components/Nav.Dropdown.jsx
+++ src/components/Nav.Dropdown.jsx
@@ -4,7 +4,8 @@ import './Nav.Dropdown.css'
 export default function Dropdown(props) {
 
   const {
-    open
+    open,
+    page
   } = props;
 
   return (
@@ -22,18 +23,21 @@ export default function Dropdown(props) {
           <a
             className="dropdown-link dropdown-link1"
             href="/about"
+            style={page === 'about' ? {color: "white", fontWeight: 700} : null}
           >
             <p className='dropdown-link-text dropdown-link-text1'>About</p>
           </a>
           <a
             className="dropdown-link dropdown-link2"
             href="/projects"
+            style={page === 'projects' ? {color: "white", fontWeight: 700} : null}
           >
             <p className='dropdown-link-text dropdown-link-text2'>Projects</p>
           </a>
           <a
             className="dropdown-link dropdown-link3"
             href="/contact"
+            style={page === 'contact' ? {color: "white", fontWeight: 700} : null}
           >
             <p className='dropdown-link-text dropdown-link-text2'>Contact</p>
           </a>
