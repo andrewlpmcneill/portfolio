efec3bf089f775c7959aed43ae0e9408e4cd4051
Andrew McNeill <andrew@andrewmc.ca>
2022-05-22 14:38:20 -0300

    creates Skills styles

---
+++ src/components/index.Skills.styles.jsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const Skill = styled.article`
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  width: 312px;
+  height: 160px;
+  background-color: rgba(255, 255, 255, 0.05);
+  border-radius: 12px;
+  padding: 40px;
+`;
+
+export const H2 = styled.h2`
+  color: rgb(255, 255, 255);
+  font-size: 18px;
+  line-height: 28px;
+  font-weight: 400;
+`;
+
+export const P = styled.p`
+  color: #9ca3af;
+  margin-top: 1.5rem;
+`;
+
+export const Circle = styled.div`
+  background-color: rgba(52, 52, 52);                                  color: white;
+  font-size: 20px;
+  border-radius: 50%;
+  height: 32px;
+  width: 32px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const A = styled.a`
+  /* text-decoration: none; */
+  color: #9ca3af;
+`;
