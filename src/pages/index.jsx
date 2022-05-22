cc336ffa9c706ecfda7a87bdad2596ed3d2326aa
Andrew McNeill <andrew@andrewmc.ca>
2022-05-22 14:39:45 -0300

    adds styled components tags

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,48 +1,52 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import Skills from '../components/index.Skills';
-import { RiReactjsFill } from 'react-icons/ri';
-import { DiNodejs } from 'react-icons/di';
-import { SiRubyonrails } from 'react-icons/si';
+import ReactJS from '../assets/reactjs.svg';
+import Node from '../assets/nodejs.svg';
+import Express from '../assets/express.svg';
+import RubyOnRails from '../assets/rubyonrails.svg';
+import PostgreSQL from '../assets/postgresql.svg';
+import MongoDB from '../assets/mongodb.svg';
+import { Main, Technologies, Technology, Introduction, H1, P, Section } from '../components/index.styles';
 
 export default function Home() {
 
   return (
   
     <Layout>
-      <main>
-        <aside className='technologies'>
-          <h3 className='technologies-react'>
-            <RiReactjsFill /> React
-          </h3>
-          <h3 className='technologies-node'>
-            <DiNodejs />
-          </h3>
-          <h3 className='technologies-express'>
-            Express
-          </h3>
-          <h3 className='technologies-rails'>
-            <SiRubyonrails />
-          </h3>
-          <h3 className='technologies-postgres'>
-            PostgreSQL
-          </h3>
-          <h3 className='technologies-mongo'>
-            MongoDB
-          </h3>
-        </aside>
-        <header className='introduction'>
-          <h1 className='introduction-heading'>
-            Based in Halifax, Nova Scotia
-          </h1>
-          <p className='introduction-paragraph'>
-            I spend my free time cycling, sailing, and having fun with microcontrollers.
-          </p>
-        </header>
-        <section className='skills'>
+      <Main>
+        <Technologies>
+          <Technology>
+            <ReactJS height="32" width="120" viewBox="47 19 50 23"/>
+          </Technology>
+          <Technology>
+            <Node height="32" width="120" viewBox="55 12 10 35" />
+          </Technology>
+          <Technology>
+            <Express height="32" width="125" viewBox="35 12 50 29" style={{marginTop: "8px"}}/>
+          </Technology>
+          <Technology>
+            <RubyOnRails height="32" width="100"/>
+          </Technology>
+          <Technology>
+            <PostgreSQL height="32" width="160" viewBox="110 -5 100 60" style={{marginTop: "3px"}}/>
+          </Technology>
+          <Technology>
+            <MongoDB height="32" width="140" viewBox="43 19 50 23" style={{marginTop: "5px"}}/>
+          </Technology>
+        </Technologies>
+        <Introduction>
+          <H1>
+            Hey, I&apos;m Andrew.
+          </H1>
+          <P>
+            I&apos;m a full-stack web developer pivoting from seven years in sales, marketing, and operations leadership.<br />Recent Lighthouse Labs graduate with a focus on JavaScript, React, NodeJS, Express, PostgreSQL, Ruby and Rails.
+          </P>
+        </Introduction>
+        <Section>
           <Skills />
-        </section>
-      </main>
+        </Section>
+      </Main>
     </Layout>
   
   )
