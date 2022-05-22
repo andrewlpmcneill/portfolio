1bb1d8ef8af42ee2e2364bdaa9c80ececcf21092
Andrew McNeill <andrew@andrewmc.ca>
2022-05-22 20:02:49 -0300

    adds styled components and finalizes content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,85 +1,117 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { Main, Header, H1, H2, Span, H3, P, A, Section, Item, Italics, SubHeading } from '../components/about.styles';
+import pattern from '../../static/pattern.svg';
 
 export default function About() {
   
   return (
 
-    <Layout>
-      <main>
-        <header>
-          <h1 className='header-heading'>
-            About me
-          </h1>
-          <h3 className='header-subheading'>
+    <Layout
+      title={false}
+      page="about"
+    >
+      <Main>
+        <Header
+          style={{backgroundImage: `linear-gradient(to bottom, rgba(17,17,17,0) 15%, rgba(17,17,17,1)), url(${pattern})`}}
+        >
+          <H1>
+            About<br /><Span>Me</Span>
+          </H1>
+          <H3>
             Web developer &amp; serial tinkerer
-          </h3>
-          <p className='paragraph header-paragraph'>
-            Hey, I&apos;m Andrew! I&apos;m a full-stack web developer based out of Halifax, Nova Scotia. Welcome to my website.<br /><br />Check out the <a className='header-paragraph-link' href='/projects'>projects</a> page to see highlights of what I&apos;ve made.
-          </p>
-        </header>
-        <section className='projects'>
-          <h2 className='heading projects-heading'>
-            Project&apos;s I&apos;m working on
-          </h2>
-          <ul className='list projects-list'>
-            <li className='list-item projects-list-item'>
-              project x...
-            </li>
-            <li className='list-item projects-list-item'>
-              chipping away at a <a className='link projects-list-link' href='https://www.amazon.ca/hz/wishlist/ls/21C3EZ68LHXV8/ref=nav_wishlist_lists_1'>CS fundamentals reading list</a>
-            </li>
+          </H3>
+          <P>
+            Hey, I&apos;m Andrew! I&apos;m a full-stack web developer based out of Halifax, Nova Scotia, Canada.<br /><br />Check out the <A href='/projects'>projects</A> page to see highlights of what I&apos;ve made.
+          </P>
+        </Header>
+        <Section>
+          <H2>
+            Projects I&apos;m working on
+          </H2>
+          <Italics>
+            Updated May 22, 2022
+          </Italics>
+          <ul
+            style={{listStyle: "none"}}
+          >
+            <Item>
+              a React Native mobile app for drywallers/tapers to expedite board counts and estimates
+            </Item>
+            <Item>
+              chipping away at a <A href='https://www.amazon.ca/hz/wishlist/ls/21C3EZ68LHXV8/ref=nav_wishlist_lists_1' target="_blank" rel="noreferrer">CS fundamentals reading list</A> <br />(currently: <A href='https://www.amazon.ca/Structure-Interpretation-Computer-Programs-JavaScript/dp/0262543230/ref=sr_1_3?keywords=structure+and+interpretation+of+computer+programs&qid=1653258342&sprefix=structure+and+in%2Caps%2C78&sr=8-3' target="_blank" rel="noreferrer">Structure and Interpretation of Computer Programs</A>)
+            </Item>
           </ul>
-        </section>
-        <section className='connect'>
-          <h2 className='heading connect-heading'>
+        </Section>
+        <Section>
+          <H2>
             Connect
-          </h2>
-          <ul className='list connect-list'>
-            <li className='list-item connect-list-item'>
-              <a className='link connect-list-link' href='andrew@andrewmc.ca'>Email</a>
-            </li>
-            <li className='list-item connect-list-item'>
-              <a className='link connect-list-link' href='https://github.com/andrewlpmcneill'>GitHub</a>
-            </li>
-            <li className='list-item connect-list-item'>
-              <a className='link connect-list-link' href='https://www.linkedin.com/in/andrewlpmcneill/'>LinkedIn</a>
-            </li>
-            <li className='list-item connect-list-item'>
-              <a className='link connect-list-link' href='https://www.strava.com/athletes/5026433'>Strava</a>
-            </li>
+          </H2>
+          <SubHeading>
+            You can always contact me at <A href='andrew@andrewmc.ca'>andrew@andrewmc.ca</A> to say hello &#8212; I love meeting new people!
+          </SubHeading>
+          <ul
+            style={{listStyle: "none"}}
+          >
+            <Item>
+              <A href='https://github.com/andrewlpmcneill' target="_blank" rel="noreferrer">GitHub</A>
+            </Item>
+            <Item>
+              <A href='https://www.linkedin.com/in/andrewlpmcneill/' target="_blank" rel="noreferrer">LinkedIn</A>
+            </Item>
+            <Item>
+              <A href='https://www.strava.com/athletes/5026433' target="_blank" rel="noreferrer">Strava</A>
+            </Item>
           </ul>
-        </section>
-        <section className='tools'>
-          <h2 className='heading tools-heading'>
+        </Section>
+        <Section>
+          <H2>
             Tools
-          </h2>
-          <h3 className='subheading tools-subheading'>
+          </H2>
+          <SubHeading
+            style={{fontWeight: "700", fontSize: "20px", marginBottom: "0"}}
+          >
             Software
-          </h3>
-          <ul className='list tools-list software-list'>
-            <li className='list-item tools-list-item software-list-item'>
-              This website is hosted on <a className='link tools-link software-link' href='https://www.rochen.com/'>Rochen</a> and uses the <a className='link tools-link software-link' href='https://www.gatsbyjs.com/'>Gatsby</a> framework
-            </li>
-            <li className='list-item tools-list-item software-list-item'>
-              <strong>Editor:</strong>&nbsp;<a className='link tools-link software-link' href='https://code.visualstudio.com/'>Visual Studio Code</a> with <a className='link tools-link software-link' href='https://marketplace.visualstudio.com/items?itemName=hugolcouto.dark-mint-vscode-theme'>Dark Mint Theme</a>
-            </li>
-            <li className='list-item tools-list-item software-list-item'>
-              <strong>Terminal:</strong>&nbsp;<a className='link tools-link software-link' href='https://iterm2.com/'>iTerm2</a>
-            </li>
+          </SubHeading>
+          <ul
+            style={{listStyle: "none"}}
+          >
+            <Item>
+              This website is hosted on <A href='https://www.rochen.com/' target="_blank" rel="noreferrer">Rochen</A> and uses the <A href='https://www.gatsbyjs.com/' target="_blank" rel="noreferrer">Gatsby</A> framework
+            </Item>
+            <Item>
+              <strong>Editor:</strong>&nbsp;<A href='https://code.visualstudio.com/'>Visual Studio Code</A> with <A href='https://marketplace.visualstudio.com/items?itemName=hugolcouto.dark-mint-vscode-theme'>Dark Mint Theme</A>
+            </Item>
+            <Item>
+              <strong>Terminal:</strong>&nbsp;<A href='https://iterm2.com/'>iTerm2</A>
+            </Item>
           </ul>
-        </section>
-        <section className='misc'>
-          <h2 className='heading misc-heading'>
+          <SubHeading
+            style={{fontWeight: "700", fontSize: "20px", marginBottom: "0"}}
+          >
+            Hardware
+          </SubHeading>
+          <ul
+            style={{listStyle: "none"}}
+          >
+            <Item>
+              <strong>Coding PC:</strong>&nbsp;M1 MacBook Air
+            </Item>
+          </ul>
+        </Section>
+        <Section>
+          <H2>
             Misc
-          </h2>
-          <ul className='list misc-list'>
-            <li className='list-item misc-list-item'>
-              <a className='link misc-link' href='https://resume.creddle.io/resume/gfxjxtbqbf0'>Resume</a>
-            </li>
+          </H2>
+          <ul
+            style={{listStyle: "none"}}         
+          >
+            <Item>
+              <A href='https://resume.creddle.io/resume/gfxjxtbqbf0' target="_blank" rel="noreferrer">Resume</A>
+            </Item>
           </ul>
-        </section>
-      </main>
+        </Section>
+      </Main>
     </Layout>
 
   )
