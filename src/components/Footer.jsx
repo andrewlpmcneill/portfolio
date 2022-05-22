de06ecef9b1950cbdf34efa0232b8b9f870c3dec
Andrew McNeill <andrew@andrewmc.ca>
2022-05-21 22:53:28 -0300

    adds styled-component tags

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,28 +1,38 @@
 import React from 'react';
 import { RiGatsbyFill } from 'react-icons/ri';
 import { AiFillGithub } from 'react-icons/ai';
+import { StyledFooter, H1, Links, Link, LinkIcon, Title } from './Footer.styles';
 
 export default function Footer() {
 
   return (
 
-    <footer>
-      <h1 className='title'>
-        Made by Andrew McNeill
-      </h1>
-      <div className='links'>
-        <a className='link link-gatsby' href='https://www.gatsbyjs.com/'>
-          <h1 className='link-title'>
-            Gatsby <RiGatsbyFill />
-          </h1>
-        </a>
-        <a className='link link-github' href='https://github.com/andrewlpmcneill'>
-          <h1 className='link-title'>
-            GitHub <AiFillGithub />
-          </h1>
-        </a>
-      </div>
-    </footer>
+    <StyledFooter>
+      <Title>
+        <H1>
+          Made by
+        </H1>
+        <H1 color={true}>
+          Andrew McNeill
+        </H1>
+      </Title>
+      <Links>
+        <Link
+          href='https://www.gatsbyjs.com/'
+        >
+          <H1>
+            Gatsby <LinkIcon><RiGatsbyFill /></LinkIcon>
+          </H1>
+        </Link>
+        <Link
+          href='https://github.com/andrewlpmcneill/portfolio'
+        >
+          <H1>
+            GitHub <LinkIcon><AiFillGithub /></LinkIcon>
+          </H1>
+        </Link>
+      </Links>
+    </StyledFooter>
   
   )
   
