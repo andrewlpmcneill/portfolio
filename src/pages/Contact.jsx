22d05374b9d3633b2b2b7e7772639c948ef73e45
Andrew McNeill <andrew@andrewmc.ca>
2022-05-22 20:40:59 -0300

    adds styled component tags and finalizes content

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { Main, Header, H1, Span, Section, A, Item } from '../components/contact.styles';
+import pattern from '../../static/pattern.svg';
 
 export default function Contact() {
 
@@ -8,20 +10,39 @@ export default function Contact() {
     <Layout
       page='contact'
     >
-      <main>
-        <h1 className='heading'>
-          Contact
-        </h1>
-        <p className='paragraph'>
-          <a className='paragraph-link' href='andrew@andrewmc.ca'>andrew@andrewmc.ca</a>
-        </p>
-        <p className='paragraph'>
-          <a className='paragraph-link' href='https://github.com/andrewlpmcneill'>GitHub</a>
-        </p>
-        <p className='paragraph'>
-          <a className='paragraph-link' href='https://www.linkedin.com/in/andrewlpmcneill/'>LinkedIn</a>
-        </p>
-      </main>
+      <Main>
+        <Header
+          style={{backgroundImage: `linear-gradient(to bottom, rgba(17,17,17,0) 15%, rgba(17,17,17,1)), url(${pattern})`}}
+        >
+          <H1>
+            Cont<Span>act</Span><br />Me
+          </H1>
+
+        </Header>
+        <Section>
+          <ul
+              style={{listStyle: "none", paddingLeft: "0"}}
+            >
+            <Item>
+              <A href='andrew@andrewmc.ca'>andrew@andrewmc.ca</A>
+            </Item>
+          </ul>
+          <ul
+              style={{listStyle: "none", paddingLeft: "0"}}
+            >
+            <Item>
+              <A href='https://github.com/andrewlpmcneill' target="_blank" rel="noreferrer">GitHub</A>
+            </Item>
+          </ul>
+          <ul
+              style={{listStyle: "none", paddingLeft: "0"}}
+            >
+            <Item>
+              <A href='https://www.linkedin.com/in/andrewlpmcneill/' target="_blank" rel="noreferrer">LinkedIn</A>
+            </Item>
+          </ul>
+        </Section>
+      </Main>
     </Layout>
 
   )
