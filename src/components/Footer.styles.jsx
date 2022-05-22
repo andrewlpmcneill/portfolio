040394a676c48f4c3f048613dd4fcef6c7ede7b9
Andrew McNeill <andrew@andrewmc.ca>
2022-05-22 16:20:29 -0300

    adds centering and changed padding

--- src/components/Footer.styles.jsx
+++ src/components/Footer.styles.jsx
@@ -3,10 +3,10 @@ import styled from 'styled-components';
 export const StyledFooter = styled.footer`
   display: flex;
   flex-direction: column;
-  align-items: left;
+  align-items: center;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   background-color: #171717;
-  padding: 1.7rem 0 0.6rem 1.5rem;
+  padding: 1.5rem 0 0.6rem 1.5rem;
   border-top: 1px solid #2a2a2a;
 `;
 
