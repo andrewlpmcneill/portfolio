c68e0943582089421fe2903984cd4fccad0593b4
Andrew McNeill <andrew@andrewmc.ca>
2022-05-22 14:38:05 -0300

    adds styled component tags

--- src/components/index.Skills.jsx
+++ src/components/index.Skills.jsx
@@ -1,40 +1,82 @@
 import React from 'react';
+import { Skill, H2, P, Circle, A } from './index.Skills.styles';
 import { IoIosPeople } from 'react-icons/io';
-import { BsArrowRepeat } from 'react-icons/bs';
+import { BsArrowRepeat, BsBookHalf } from 'react-icons/bs';
 import { GiChemicalDrop } from 'react-icons/gi';
+import { RiTestTubeFill } from 'react-icons/ri';
+import { FaLightbulb } from 'react-icons/fa';
 
 export default function Skills() {
 
   return (
 
     <>
-      <article className='skill skill-people'>
-        <IoIosPeople />
-        <h2 className='skill-header skill-header-people'>
+      <Skill>
+        <Circle>
+          <IoIosPeople/>
+        </Circle>
+        <H2>
           Effective communicator
-        </h2>
-        <p className='skill-paragraph skill-paragraph-people'>
+        </H2>
+        <P>
           Over a decade of client-facing professional experience.
-        </p>
-      </article>
-      <article className='skill skill-starter'>
-        <BsArrowRepeat />
-        <h2 className='skill-header skill-header-starter'>
+        </P>
+      </Skill>
+      <Skill>
+        <Circle>
+          <BsArrowRepeat />
+        </Circle>
+        <H2>
           Self starter
-        </h2>
-        <p className='skill-paragraph skill-paragraph-starter'>
+        </H2>
+        <P>
           New to development, not to problem solving.
-        </p>
-      </article>
-      <article className='skill skill-adaptable'>
-        <GiChemicalDrop />
-        <h2 className='skill-header'>
+        </P>
+      </Skill>
+      <Skill>
+        <Circle>
+          <GiChemicalDrop />
+        </Circle>
+        <H2>
           Easy to work with
-        </h2>
-        <p className='skill-paragraph skill-paragraph-starter'>
-          Open to all of the many ways to solve a problem
-        </p>
-      </article>
+        </H2>
+        <P>
+          Open to all of the many ways to solve a problem.
+        </P>
+      </Skill>
+      <Skill>
+        <Circle>
+          <FaLightbulb />
+        </Circle>
+        <H2>
+          Curious by nature
+        </H2>
+        <P>
+          Fascinated by the possible solutions programming and technology can provide.
+        </P>
+      </Skill>
+      <Skill>
+        <Circle>
+          <RiTestTubeFill />
+        </Circle>
+        <H2>
+          Test-driven developer
+        </H2>
+        <P>
+          Believes in unit, integration, and end-to-end testing in balance.
+        </P>
+      </Skill>
+      <Skill>
+        <Circle>
+          <BsBookHalf />
+        </Circle>
+        <H2>
+          Not just a tool user
+        </H2>
+        <P>
+          Committed to a life-long study of <A href="https://teachyourselfcs.com/" target="_blank" rel="noreferrer">computer science</A>.
+        </P>
+      </Skill>
     </>
   
   )
