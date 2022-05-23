1011c16219223d7edcdc3d3b67536a34090dfb90
Andrew McNeill <andrew@andrewmc.ca>
2022-05-23 10:00:45 -0300

    adds styled components tags

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import ProjectsList from '../components/projects.projectsList';
+import { Main, Header, H1, Span, H3, A } from '../components/projects.styles';
+import pattern from '../../static/pattern.svg';
 
 export default function Projects() {
 
@@ -9,15 +11,19 @@ export default function Projects() {
     <Layout
       page='projects'
     >
-      <main>
-        <h1 className='header-heading'>
-          Projects
-        </h1>
-        <h3 className='header-subheading'>
-          View them all <a className='header-subheading-link' href='https://github.com/andrewlpmcneill?tab=repositories'>on GitHub</a>
-        </h3>
+      <Main>
+        <Header
+          style={{backgroundImage: `linear-gradient(to bottom, rgba(17,17,17,0) 15%, rgba(17,17,17,1)), url(${pattern})`}}
+        >
+          <H1>
+            Selected<br /><Span>Projects</Span>
+          </H1>
+          <H3>
+            View them all <A href='https://github.com/andrewlpmcneill?tab=repositories' target="_blank" rel="noreferrer">on GitHub</A>
+          </H3>
+        </Header>
         <ProjectsList />
-      </main>
+      </Main>
     </Layout>
 
   )
