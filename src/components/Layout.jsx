172f3e96186567746c4ac7e97ffa723563f5c030
Andrew McNeill <andrew@andrewmc.ca>
2022-05-26 11:23:30 -0300

    adds mood state and side effect handling

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Nav from './Nav';
 import Header from './Header';
 import Footer from './Footer';
-import './Layout.css'
+import './Layout.css';
+
+export const MoodContext = React.createContext("");
 
 export default function Layout(props) {
 
@@ -12,18 +14,29 @@ export default function Layout(props) {
     page
   } = props;
 
+  const [mood, setMood] = useState(localStorage.getItem("mood") || "euphoria");
+
+  useEffect(() => {
+    localStorage.setItem("mood", mood)
+  }, [mood]);
+
   return (
 
-    <>
+    <MoodContext.Provider
+      value={mood}
+    >
       <Nav
         page={page}
+        mood={mood}
+        setMood={setMood}
       />
       <Header
         title={title}
+        mood={mood}
       />
       {children} 
       <Footer />
-    </>
+    </MoodContext.Provider>
   
   )
   
