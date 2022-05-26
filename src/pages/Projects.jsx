8a3a3e8784f99dedd08e5ca6f73459b8104b1bcf
Andrew McNeill <andrew@andrewmc.ca>
2022-05-26 11:28:05 -0300

    moves project page content to projectComponent

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import ProjectsList from '../components/projects.projectsList';
-import { Main, Header, H1, Span, H3, A } from '../components/projects.styles';
-import pattern from '../../static/pattern.svg';
+import ProjectsComponent from '../components/projectsComponent';
 
 export default function Projects() {
 
@@ -11,19 +9,7 @@ export default function Projects() {
     <Layout
       page='projects'
     >
-      <Main>
-        <Header
-          style={{backgroundImage: `linear-gradient(to bottom, rgba(17,17,17,0) 15%, rgba(17,17,17,1)), url(${pattern})`}}
-        >
-          <H1>
-            Selected<br /><Span>Projects</Span>
-          </H1>
-          <H3>
-            View them all <A href='https://github.com/andrewlpmcneill?tab=repositories' target="_blank" rel="noreferrer">on GitHub</A>
-          </H3>
-        </Header>
-        <ProjectsList />
-      </Main>
+      <ProjectsComponent />
     </Layout>
 
   )
